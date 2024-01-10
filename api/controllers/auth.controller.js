ae24032b085519793a54aba266fcc52eaf0922fe
SamyakMahajan <ketann682@gmail.com>
2024-01-10 18:03:07 +0530

    create signup API route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+import User from '../models/user.model.js'
+import bcryptjs from 'bcryptjs'
+export const signup= async (req,res)=> {
+    console.log(req.body);
+    const {username,email,password}=req.body;
+    const hashedPassword=bcryptjs.hashSync(password,10);
+    const newUser=new User({username,email,password:hashedPassword});
+    try
+    {await newUser.save()
+    // res.send
+    res.status(201).json({
+        "message":`user created successfully!!`
+    })}
+    catch(err){
+        res.status(500).json(err.message);
+    }
+}
--- api/index.js
+++ api/index.js
@@ -1,18 +1,22 @@
 // const express = require('express')
 import express from 'express';
 const app = express()
+app.use(express.json())
 import mongoose from 'mongoose';
 // const mongoose =require('mongoose')
 const port= 3400
 import dotenv from 'dotenv'
 // const dotenv = require('dotenv');
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 dotenv.config();
 mongoose.connect(process.env.MONGODB_URI).then(()=>{console.log("Connected!")}).catch((err)=>{console.log(err);});
 app.get('/', (req, res) => {
   res.json({message:'Hello World!'})
 })
 app.use('/api/user',userRoutes)
+
+app.use('/api/auth',authRoutes)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
